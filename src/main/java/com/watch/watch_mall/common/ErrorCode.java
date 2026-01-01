d6f355d413e502141c7bb5d888f0bf8e4bea5553
Truth <2488292668@qq.com>
2026-01-01 22:16:45 +0800

    refactor(ErrorCode): ErrorCode使用Getter生成get方法

--- src/main/java/com/watch/watch_mall/common/ErrorCode.java
+++ src/main/java/com/watch/watch_mall/common/ErrorCode.java
@@ -1,10 +1,13 @@
 package com.watch.watch_mall.common;
 
+import lombok.Getter;
+
 /**
  * 自定义错误码
  *
 
  */
+@Getter
 public enum ErrorCode {
 
     SUCCESS(0, "ok"),
@@ -29,14 +32,6 @@ public enum ErrorCode {
     ErrorCode(int code, String message) {
         this.code = code;
         this.message = message;
-    }
-
-    public int getCode() {
-        return code;
-    }
-
-    public String getMessage() {
-        return message;
     }
 
 }
