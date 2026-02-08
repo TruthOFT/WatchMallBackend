5b6bd7c6d30ea3b93111c20862cc38fc6beaa9ad
Truth <2488292668@qq.com>
2026-02-08 21:30:18 +0800

    :recycle: refactor: 移除了choiceList

--- src/main/java/com/watch/watch_mall/model/vo/HomeProductVO.java
+++ src/main/java/com/watch/watch_mall/model/vo/HomeProductVO.java
@@ -9,7 +9,5 @@ public class HomeProductVO {
     List<CategoryVO> categoryVOList;
     ProductVO productVO;
     List<ProductVO> bannerList;
-    List<ProductVO> choiceList;
     List<ProductVO> recommendList;
-
 }
